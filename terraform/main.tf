b80cd50ac7808535ea984f7334014f669d736ae1
Sarah Cantu <sarah.cantu.consulting@gmail.com>
2025-11-11 21:28:15 -0600

    attempting to standup up terraform eks etc, but ran into a problem with the ebs-csi version and eks 1.29 -- consider upgrading

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ module "vpc" {
   version = "~> 5.0"
 
   name               = local.name
-  cidr               = "10.0.00/16"
+  cidr               = "10.0.0.0/16"
   azs                = ["${var.region}a","${var.region}b"]
   private_subnets    = ["10.0.1.0/24","10.0.2.0/24"]
   public_subnets     = ["10.0.101.0/24","10.0.102.0/24"]
@@ -38,21 +38,35 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
   vpc_id     = module.vpc.vpc_id
 
+  enable_irsa                             = true
+  enable_cluster_creator_admin_permissions = true
+  
   eks_managed_node_groups = {
     default = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 1
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
+      labels         = { role = "workers" }
       tags           = local.tags
     }
+  }
+
+  # Core addons (managed by AWS)
+  cluster_addons = {
+    vpc-cni   = { most_recent = true }
+    coredns   = { most_recent = true }
+    kube-proxy= { most_recent = true }
+    ebs-csi   = { most_recent = true }
   }
 
   tags = local.tags
 }
 
-# (Optional) ECR repository for image builds (demo)
+
+
+# ECR repository for image builds
 resource "aws_ecr_repository" "ml_service" {
   name                 = "${local.name}-ml-service"
   image_scanning_configuration { scan_on_push = true }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,19 @@
-variable "region"     { type = string  default = "us-east-1" }
-variable "project"    { type = string  default = "mlops-starter" }
-variable "env"        { type = string  default = "demo" }
-variable "cluster_version" { type = string default = "1.29" }
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "project" {
+  type    = string
+  default = "mlops-starter"
+}
+
+variable "env" {
+  type    = string
+  default = "demo"
+}
+
+variable "cluster_version" {
+  type    = string
+  default = "1.29"
+}
