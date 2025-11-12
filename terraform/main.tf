23662dfb588901bfc4facef95a2e6a8aca62fd77
Sarah Cantu <sarah.cantu.consulting@gmail.com>
2025-11-11 21:46:23 -0600

    formatting fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,9 +19,9 @@ module "vpc" {
 
   name               = local.name
   cidr               = "10.0.0.0/16"
-  azs                = ["${var.region}a","${var.region}b"]
-  private_subnets    = ["10.0.1.0/24","10.0.2.0/24"]
-  public_subnets     = ["10.0.101.0/24","10.0.102.0/24"]
+  azs                = ["${var.region}a", "${var.region}b"]
+  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24"]
   enable_nat_gateway = true
   single_nat_gateway = true
   tags               = local.tags
@@ -38,9 +38,9 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
   vpc_id     = module.vpc.vpc_id
 
-  enable_irsa                             = true
+  enable_irsa                              = true
   enable_cluster_creator_admin_permissions = true
-  
+
   eks_managed_node_groups = {
     default = {
       min_size       = 1
@@ -55,9 +55,9 @@ module "eks" {
 
   # Core addons (managed by AWS)
   cluster_addons = {
-    vpc-cni   = { most_recent = true }
-    coredns   = { most_recent = true }
-    kube-proxy= { most_recent = true }
+    vpc-cni    = { most_recent = true }
+    coredns    = { most_recent = true }
+    kube-proxy = { most_recent = true }
   }
 
   tags = local.tags
@@ -67,7 +67,7 @@ module "eks" {
 
 # ECR repository for image builds
 resource "aws_ecr_repository" "ml_service" {
-  name                 = "${local.name}-ml-service"
+  name = "${local.name}-ml-service"
   image_scanning_configuration { scan_on_push = true }
   tags = local.tags
 }
